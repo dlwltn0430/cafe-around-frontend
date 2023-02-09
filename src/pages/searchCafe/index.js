ad22412937a7a73109ba8e27b896a0f370ab7f3d
Jisu Lee <dlwltn0430@naver.com>
2023-02-10 03:31:52 +0900

    style: styled-components 적용

--- src/pages/searchCafe/index.js
+++ src/pages/searchCafe/index.js
@@ -1,15 +1,20 @@
 import { useState, useCallback, useId } from 'react';
 import './index.css';
 import { projects } from './fakeDB.js';
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
-function App() {
+function SearchCafe() {
   //'콘센트가 많은', '테이블이 넓은', '좌석이 많은', '스터디룸이 있는'
   const [tags, setTags] = useState(['콘센트가 많은']);
+  const [univ, setUniv] = useState(['부산대']);
 
   //const id = useId();
 
   const addTag = useCallback(
     (tag) => () => {
+      console.log(tags);
+      console.log(tag);
       if (!tags.includes(tag)) {
         return setTags((prevTags) => [...prevTags, tag]);
       }
@@ -17,19 +22,61 @@ function App() {
     [tags]
   );
 
+  // 수정
+  const addUniv = useCallback(
+    (u) => () => {
+      console.log(univ);
+      console.log(u);
+      if (!univ.includes(u)) {
+        return setUniv((prevUniv) => [...prevUniv, u]);
+      }
+    },
+    [univ]
+  );
+
+
   const deleteTag = useCallback(
     (tagId) => () => {
+      console.log(tagId); // 테이블이 넓은
+      console.log(tags); // ['테이블이 넓은']
       const tagsFiltered = tags.filter((tag) => {
         return tag !== tagId;
+        
       });
+      console.log(tagsFiltered); // []
       setTags(tagsFiltered);
     },
     [tags]
   );
 
+  console.log(tags);
+  console.log(univ);
+  
+  // 수정
+  const deleteUniv = useCallback(
+    (univId) => () => {
+      console.log(univId); // 부산대
+      console.log(univ); // 부산대
+      const univFiltered = univ.filter((u) => {
+        return u !== univId; 
+      });
+      /*const univFiltered = univ !== univId;*/
+      console.log(univFiltered);
+      setUniv(univFiltered);
+    },
+    [univ]
+  );
+
+  
   const matchTags = (current, target) => {
     return target.every((tag) => current.includes(tag));
   };
+
+  //수정
+  const matchUniv = (current, target) => {
+    return target.every((univ) => current.includes(univ));
+  };
+
 
   return (
     <div className='tags_container'>
@@ -37,8 +84,10 @@ function App() {
       <div>
         {tags.length > 0
           ? tags.map((tag) => {
+            
                 return (
-                    <button
+                  
+                    <button id={tag}
                       className='close'
                       onClick={deleteTag(tag)}
                     >
@@ -47,31 +96,123 @@ function App() {
                   );
             })
           : '원하는 태그를 선택해주세요!'}
+          
       </div>
-      {projects
-        .filter((proj) => matchTags(proj.tags, tags))
-        .map(({ id, title, description, tags }) => {
-          return (
-            <div className='card'>
-              <div>
-                <p style={{fontWeight : "800"}}>{title}</p>
-                <p>{description}</p>
-              </div>
+
+    {/*수정*/ }
+      <div>
+      {univ.length > 0
+          ? univ.map((u) => {
+            
+                return (
+                  
+                    <button id={u}
+                      className='close-1'
+                      onClick={deleteUniv(u)}
+                    >
+                      {u} &nbsp; x
+                    </button>
+                  );
+            })
+          : '원하는 대학교를 선택해주세요!'}
+      </div>
+
+      <ListWrapper>
+        {projects
+          .filter((proj) => matchUniv(proj.univ, univ))
+          .filter((proj) => matchTags(proj.tags, tags))
+          .map(({ id, title, address, tags, latitude, longitude, univ, price, openingHour }) => {
+            return (
+              <ListCard key={id}>
+
+              {/*<Link to = "/detail" state={{ latitude: latitude, longitude: longitude }} style={{textDecoration: 'none', color: "#000", flex: 1}}>
+                <div>
+                  <p style={{fontWeight : "800"}}>{title}</p>
+                  <p>{description}</p>
+                </div>
+            </Link>*/}
+
+                <div>
+                  <p style={{fontWeight : "800"}}>{title}</p>
+                  <p>{address}</p>
+                </div>
+              <div style={{height: 'inherit'}}>
               {tags.map((tag) => {
                 return (
-                  <button
-                    type='button'
+                  <Tags id={tag}
                     onClick={addTag(tag)}
                   >
                     #{tag}
-                  </button>
+                  </Tags>
                 );
               })}
-            </div>
-          );
-        })}
+              </div>
+
+              <div style={{height: 'inherit'}}>
+              
+          
+                  <button id={univ}
+                    onClick={addUniv(univ)}>
+                    #{univ}
+                  </button>
+                
+              </div>
+
+              <Link to="/detail" 
+                    state={{ title: title, address: address, tags: tags, latitude: latitude, longitude: longitude, univ: univ, price: price, openingHour: openingHour }} 
+                    style={{textDecoration: 'none', color: "#000", flex: 1}}>
+              상세보기
+              </Link>
+            
+              </ListCard>
+            );
+          })}
+      </ListWrapper>
     </div>
   );
 }
 
-export default App;
+
+const ListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  width: 800px;
+`;
+
+const ListCard = styled.div`
+  width: 320px;
+  padding: 1rem;
+  margin: 1rem;
+
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 8px;
+  border-radius: 4px;
+
+
+  @media (max-width: 800px) {
+    width: 100%;
+  }
+`;
+
+const Tags = styled.button`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  height: 2rem;
+  display: -webkit-inline-box;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  margin-right: 0.875rem;
+  margin-bottom: 1rem;
+  color: #6e5eff; 
+  font-weight: 500;
+  font-size: 1rem;
+  background: #f1f3f5;
+  border-radius: 1rem;
+  text-decoration: none;
+  border: 0;
+`;
+
+export default SearchCafe;
